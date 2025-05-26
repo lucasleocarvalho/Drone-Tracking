2f5e06820de4068d5a560019758fed93fdeeb29f
Bilola <lucasle58022@gmail.com>
2025-05-26 16:14:08 -0300

    atualizando subscriber

--- drone_tracking/tracking_subscriber.py
+++ drone_tracking/tracking_subscriber.py
@@ -1,23 +1,29 @@
 import rclpy
 from rclpy.node import Node
 import cv2 as cv
-from ultralytics import YOLO
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridge
-import os
+import numpy as np
 
 class CameraShow(Node):
     
     def __init__(self):
-        super().__init__('detection')
+        super().__init__('camera_show')
         cv.namedWindow("Detecção YOLO", cv.WINDOW_NORMAL)
         self.bridge = CvBridge()
 
-        #Criação de publishers e subscribers
-        self.camera_subscription = self.create_subscription(Image, '/camera', self.image_callback, 10)
+        # Subscriber para imagem comprimida
+        self.camera_subscription = self.create_subscription(
+            CompressedImage,
+            '/camera/compressed',
+            self.image_callback,
+            10)
     
     def image_callback(self, msg):
-        cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        # Converte imagem comprimida para cv2
+        np_arr = np.frombuffer(msg.data, np.uint8)
+        cv_image = cv.imdecode(np_arr, cv.IMREAD_COLOR)
+
         cv.imshow('Detecção YOLO', cv_image)
         if cv.waitKey(1) == ord('q'):
             self.get_logger().info('Fechando janela')
@@ -29,7 +35,6 @@ def main(args=None):
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
