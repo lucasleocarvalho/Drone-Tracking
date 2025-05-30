f0e3465ed98088a8ad18ea75b7f335df7a11cb6d
Ruzkita <lucasle58022@gmail.com>
2025-05-30 15:06:31 -0300

    YOLO implementation

--- drone_tracking/tracking.py
+++ drone_tracking/tracking.py
@@ -4,6 +4,9 @@ import cv2 as cv
 from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridge
 import os
+from ultralytics import YOLO
+from ament_index_python import get_package_share_directory
+import numpy as np
 
 
 class Detection(Node):
@@ -16,15 +19,23 @@ class Detection(Node):
         self.cap.set(cv.CAP_PROP_FRAME_WIDTH, 640)
         self.cap.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
 
+        #Import da rede neural
+        package_dir = get_package_share_directory('drone_tracking')
+        model_path = os.path.join(package_dir, 'net_train', 'weights', 'best.pt')
+        self.model = YOLO(model_path)
+
         fps = self.cap.get(cv.CAP_PROP_FPS)
         self.get_logger().info(f"FPS configurado na câmera: {fps}")
 
-        # Publisher de imagem comprimida
-        self.camera_publishing = self.create_publisher(
-            CompressedImage, '/camera/compressed', 10)
+        #Publisher de imagem
+        self.camera_publishing = self.create_publisher(CompressedImage, '/camera/compressed', 10)
         self.bridge = CvBridge()
 
-        # Timer para 30 FPS (~33ms)
+        #Variáveis de controle
+        self.frame_c = 0
+        self.annoted_frame = None
+
+        #Timer para 30 FPS
         timer_period = 1.0 / 30.0
         self.timer = self.create_timer(timer_period, self.capture_callback)
 
@@ -33,9 +44,20 @@ class Detection(Node):
         if not ret:
             self.get_logger().error("Erro ao capturar frame da câmera")
             return
+        
+        #Detecção com YOLO
+        self.frame_c += 1
+        if self.frame_c % 3 == 0:
+            results = self.model(frame, verbose=False, conf=0.5)
+            self.annoted_frame = np.array(results[0].plot())
+        #else:
+        #    self.annoted_frame = None
+
+        #Frame para exibição
+        frame_show = self.annoted_frame if self.annoted_frame is not None else frame
 
         # Converte para imagem comprimida (JPEG)
-        ros_compressed_image = self.bridge.cv2_to_compressed_imgmsg(frame, dst_format='jpeg')
+        ros_compressed_image = self.bridge.cv2_to_compressed_imgmsg(frame_show, dst_format='jpeg')
         self.camera_publishing.publish(ros_compressed_image)
 
     def destroy_node(self):
--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', glob('launch/*.py')),
         ('lib/' + package_name, glob('scripts/*.py')),  
+        (f'share/{package_name}/net_train/weights', glob('net_train/weights/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
