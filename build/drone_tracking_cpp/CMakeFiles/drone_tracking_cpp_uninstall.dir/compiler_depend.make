152b9363add1e37d134f62e9c08f3059b7e6fb90
Ruzkita <lucasle58022@gmail.com>
2025-07-09 12:54:55 -0300

    .

---
+++ build/drone_tracking_cpp/CMakeFiles/drone_tracking_cpp_uninstall.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for drone_tracking_cpp_uninstall.
+# This may be replaced when dependencies are built.
---
+++ build/drone_tracking_cpp/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lacea/ros2_ws/src/Drone-Tracking/drone_tracking_cpp
+# Build directory: /home/lacea/ros2_ws/src/Drone-Tracking/build/drone_tracking_cpp
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
